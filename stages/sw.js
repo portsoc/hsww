33440902a22c11c7808bbd402dd3c1290436514b
Rich Boakes <rjb@port.ac.uk>
2025-03-05 15:25:20 +0000

    stage 5

--- stages/sw.js
+++ stages/sw.js
@@ -1,10 +1,27 @@
-console.log("Service Worker that logs fetch events is now running.");
-
 /* Log fetch requests and then process them as normal */
 async function interceptFetch(evt) {
   console.log(evt.request.method, evt.request.url);
   return await fetch(evt.request);
 }
 
-self.addEventListener('fetch', interceptFetch);
+const CACHE = 'hsww';
+const CACHEABLE = [
+  './',
+  './index.html',
+  './sw.js',
+  './script.js',
+  './style.css',
+  './message.txt',
+  './img/192.png',
+];
 
+/* Prepare and populate a cache. */
+async function prepareCache() {
+  const c = await caches.open(CACHE);
+  await c.addAll(CACHEABLE);
+  console.log('Cache prepared.');
+}
+
+// install the event listener so it can run in the background.
+self.addEventListener('install', prepareCache);
+self.addEventListener('fetch', interceptFetch);
